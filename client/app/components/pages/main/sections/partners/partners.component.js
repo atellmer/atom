d6d2562a74c4c2eda16d1b352979aac774362887
Alex Plex <atellmer@gmail.com>
2016-06-25 13:38:23 +0500

    bootstrap carousels replaced with slick carousels

--- client/app/components/pages/main/sections/partners/partners.component.js
+++ client/app/components/pages/main/sections/partners/partners.component.js
@@ -1,6 +1,47 @@
 ;
-(function(){
-	$('#partners-carousel').carousel({
-		interval: 5000
+(function () {
+	'use strict';
+
+	$('#partners-carousel').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 640,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			}
+		]
 	});
 })();
--- client/app/components/pages/main/sections/production/production.component.js
+++ client/app/components/pages/main/sections/production/production.component.js
@@ -1,6 +1,39 @@
 ;
-(function(){
-	$('#production-carousel').carousel({
-		interval: 5000
+(function () {
+	'use strict';
+
+	$('#production-carousel').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 640,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			}
+		]
 	});
 })();
